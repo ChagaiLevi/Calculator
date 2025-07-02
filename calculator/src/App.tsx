f42c82b4f5d69615f4a7d6e2c5a5460dca8849d3
Chagai Levi <schagailevi@gmail.com>
2025-07-02 21:48:56 +0300

    Installing the math js library and calculating the exercise

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+//import React from 'react';
 import HistoryBtn from './HistoryBtn';
 import HistoryPage from './HistoryPage';
 import Calculator from './Calculator';
 import Copyright from './Copyright';
+import React, { useState, useEffect } from 'react';
+import { evaluate } from "mathjs";
 
 function App() {
+  const [exercise, setExercise] = useState<string>('');
+  const [result, setResult] = useState<number | null>(null);
+
+  useEffect(() => {
+    try {
+      const checkResult = evaluate(exercise);
+      setResult(checkResult);
+    }
+    catch (error) {
+      setResult(null);
+    }
+  });
   return (
     <div>
       <HistoryBtn />
       <HistoryPage />
-      <Calculator />
+      <Calculator exercise={exercise} setExercise={setExercise} result={result} />
       <Copyright />
     </div>
   );
--- calculator/src/Calculator.tsx
+++ calculator/src/Calculator.tsx
@@ -1,9 +1,9 @@
-const Calculator = () => {
+const Calculator: React.FC<{ exercise: string, setExercise: React.Dispatch<React.SetStateAction<string>>, result: number | null }> = ({ exercise, setExercise, result }) => {
   return (
     <div className="mainContainer">
       <div>Please enter an exercise and then press enter:</div>
-      <input type="text" className="exercise" placeholder="Type here..." maxLength={33} minLength={3} />
-      <p className="text"></p>
+      <input type="text" className="exercise" placeholder="Type here..." maxLength={33} minLength={3} onChange={(e) => setExercise(e.target.value)} />
+      <p className="text">{result ? result : 0}</p>
     </div>
   )
 }
