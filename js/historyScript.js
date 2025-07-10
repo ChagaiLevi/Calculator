f1fbeafebbf32def80e2ddd23a1f580bb207c49d
Chagai Levi <schagailevi@gmail.com>
2025-07-10 16:54:41 +0300

    Transferring files to master files

--- js/historyScript.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("historyBtn").onclick = function () {
-  document.getElementById("sidebar").style.width = "300px";
-};
-
-document.getElementById("closeBtn").onclick = function () {
-  document.getElementById("sidebar").style.width = "0";
-};
